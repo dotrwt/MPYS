a2c221906d375b8235bf49d0009f1181428880a0
Harshvardhan Rawat <fest.nov01@gmail.com>
2026-07-05 16:47:36 +0530

    Navbar size and equal spacing adjustments

--- components/layout/navbar/navbar.tsx
+++ components/layout/navbar/navbar.tsx
@@ -2,40 +2,28 @@ import React from "react";
 import "./navbar.css";
 
 export default function Navbar() {
-  return (
-    <header className="navbar-header">
-      <div className="navbar-container">
-        {/* Left: Brand Logo */}
-        <div className="navbar-brand">
-          <a href="/" className="navbar-brand-link">
-            Think Forward®
-          </a>
-        </div>
-
-        {/* Center: Navigation Link List */}
-        <nav className="navbar-nav">
-          <a href="#speakers" className="navbar-link">
-            SPEAKERS
-          </a>
-          <a href="#agenda" className="navbar-link">
-            AGENDA
-          </a>
-          <a href="#venue" className="navbar-link">
-            VENUE
-          </a>
-          <a href="#sponsors" className="navbar-link">
-            SPONSORS
-          </a>
-        </nav>
-
-        {/* Right: Get Tickets CTA */}
-        <div className="navbar-cta">
-          <a href="/registration" className="navbar-cta-link">
-            <span className="navbar-cta-arrow">→</span>
-            <span>Get tickets</span>
-          </a>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="navbar-header">
+            <div className="navbar-container">
+                <a href="/" className="navbar-item navbar-brand">
+                    Madhya Pradesh Youth Summit®
+                </a>
+                <a href="#speakers" className="navbar-item navbar-link">
+                    SPEAKERS
+                </a>
+                <a href="#agenda" className="navbar-item navbar-link">
+                    AGENDA
+                </a>
+                <a href="#venue" className="navbar-item navbar-link">
+                    VENUE
+                </a>
+                <a href="#sponsors" className="navbar-item navbar-link">
+                    SPONSORS
+                </a>
+                <a href="/registration" className="navbar-item navbar-cta">
+                    <span className="navbar-cta-arrow">→</span> Get tickets
+                </a>
+            </div>
+        </header>
+    );
 }
